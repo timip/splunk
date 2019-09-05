5697c2f3602a4b90fd52868f5d87705c001511ae
tiptiptip <2239274+timip@users.noreply.github.com>
2019-09-05 13:10:50 -0700

    Upload Splunk App for Sysmon Deployme

---
+++ uf_sysmon_config/uf_sysmon_config/bin/install_sysmon.bat
@@ -0,0 +1,55 @@
+@echo off
+
+cd %~dp0
+echo Current Directory = %cd%
+
+:check_os
+IF "%PROCESSOR_ARCHITECTURE%"=="x86" (
+    set bit=x86
+) else (
+    set bit=x64
+)
+
+:check_sysmon
+IF %bit% == "x86" (
+    sc query "Sysmon" | Find "RUNNING"
+) else (
+    sc query "Sysmon64" | Find "RUNNING"
+)
+IF "%ERRORLEVEL%" NEQ "0" (
+    goto start_sysmon
+)
+goto update_config
+
+:start_sysmon
+echo Starting sysmon...
+IF %bit% == "x86" (
+    net start Sysmon
+) else (
+    net start Sysmon64
+)
+IF "%ERRORLEVEL%" NEQ "0" (
+    goto install_sysmon
+)
+
+:update_config
+echo Updating Sysmon Config...
+IF %bit% == "x86" (
+    sysmon.exe -c sysmonconfig-export.xml
+) else (
+    sysmon64.exe -c sysmonconfig-export.xml
+)
+goto exit
+
+:install_sysmon
+echo Installing Sysmon Config...
+sysmon.exe -u
+sysmon64.exe -u
+IF %bit% == "x86" (
+    sysmon.exe /accepteula -i sysmonconfig-export.xml
+) else (
+    sysmon64.exe /accepteula -i sysmonconfig-export.xml
+)
+
+:exit
+echo Completed!
