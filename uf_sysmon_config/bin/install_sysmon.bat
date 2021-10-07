1a7863c2eb917952303e4de5eb9233fe231e2103
Tim Ip <timip@timip.org>
2021-10-07 11:31:41 -0700

    bump sysmon to 13.24 and sysmon-modular configuration to 4.60

---
+++ uf_sysmon_config/bin/install_sysmon.bat
@@ -0,0 +1,66 @@
+@echo off
+
+cd %~dp0
+echo Current Directory = %cd%
+
+:check_os
+IF "%PROCESSOR_ARCHITECTURE%"=="x86" (
+    set bit=x86
+) else (
+    set bit=x64
+)
+
+:check_sysmon
+IF %bit% == "x86" (
+    sc query "Sysmon" | Find "RUNNING"
+) else (
+    sc query "Sysmon64" | Find "RUNNING"
+)
+IF "%ERRORLEVEL%" NEQ "0" (
+    goto start_sysmon
+)
+goto check_sysmon_version
+
+:start_sysmon
+echo Starting sysmon...
+IF %bit% == "x86" (
+    net start Sysmon
+) else (
+    net start Sysmon64
+)
+IF "%ERRORLEVEL%" NEQ "0" (
+    goto install_sysmon
+)
+
+:check_sysmon_version
+IF %bit% == "x86" (
+    Sysmon | Find "13.24"
+) else (
+    Sysmon64 | Find "13.24"
+)
+IF "%ERRORLEVEL%" NEQ "0" (
+    goto install_sysmon
+)
+goto update_config
+
+:update_config
+echo Updating Sysmon Config...
+IF %bit% == "x86" (
+    sysmon.exe -c sysmonconfig-export.xml
+) else (
+    sysmon64.exe -c sysmonconfig-export.xml
+)
+goto exit
+
+:install_sysmon
+echo Installing Sysmon Config...
+sysmon.exe -u
+sysmon64.exe -u
+IF %bit% == "x86" (
+    sysmon.exe /accepteula -i sysmonconfig-export.xml
+) else (
+    sysmon64.exe /accepteula -i sysmonconfig-export.xml
+)
+
+:exit
+echo Completed!
